c2f9fc7e316941ce733a3066cee1f2e9e1430948
Vladislav Glinskiy <vglinskiy@cybervisiontech.com>
2018-09-07 13:50:28 +0300

    issue #1: Added workaround for Hive beeline issue, run.sh changed to configure the Airflow

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,21 @@ ENV SLUGIFY_USES_TEXT_UNIDECODE=yes
 RUN yum upgrade python-setuptools
 RUN yum install -y python-pip git gcc gcc-c++ libffi-devel python-devel python-pip python-wheel openssl-devel libsasl2-devel openldap-devel cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-md5 cyrus-sasl-plain
 
-RUN yum install -y mapr-spark mapr-hive mapr-hiveserver2 mapr-hivemetastore
+RUN yum install -y mapr-spark
 
-# TODO: install sqlline
+RUN mkdir /tmp/drill-rpm
+WORKDIR /tmp/drill-rpm
+RUN wget https://package.mapr.com/releases/MEP/MEP-5.0.0/redhat/mapr-drill-internal-1.13.0.201803281600-1.noarch.rpm
+RUN wget https://package.mapr.com/releases/MEP/MEP-5.0.0/redhat/mapr-drill-1.13.0.201803281600-1.noarch.rpm
+RUN rpm -Uvh mapr-drill-internal-1.13.0.201803281600-1.noarch.rpm
+RUN rpm -Uvh --nodeps mapr-drill-1.13.0.201803281600-1.noarch.rpm
+
 
 RUN pip install --upgrade setuptools pip
 RUN pip install apache-airflow apache-airflow[hive] hmsclient
+
+# Workaround for 'hive_hooks' beeline issue
+RUN sed -i -e "s/{hql}/{hql}\\\n/g" /usr/lib/python2.7/site-packages/airflow/hooks/hive_hooks.py
 
 # Create a directory for your MapR Application and copy the Application
 RUN mkdir -p /home/mapr/mapr-apps/mapr-airflow
--- bin/run.sh
+++ bin/run.sh
@@ -10,19 +10,57 @@ else
     WEB_UI_PORT=${DEFAULT_WEB_UI_PORT}
 fi
 
+# Check if 'DRILL_NODE' environment variable set
+if [ ! -z ${DRILL_NODE+x} ]; then # DRILL_NODE exists
+    echo "Drill node: $DRILL_NODE"
+else
+    echo 'DRILL_NODE environment variable is not set. Please set it and rerun.'
+    exit 1
+fi
+
+# Check if 'DRILL_CLUSTER_ID' environment variable set
+if [ ! -z ${DRILL_CLUSTER_ID+x} ]; then # DRILL_CLUSTER_ID exists
+    echo "Drill cluster id: $DRILL_CLUSTER_ID"
+else
+    echo 'DRILL_CLUSTER_ID environment variable is not set. Please set it and rerun.'
+    exit 1
+fi
+
+# Check if 'MAPR_USER_PASSWORD' environment variable set
+if [ ! -z ${MAPR_USER_PASSWORD+x} ]; then # DRILL_CLUSTER_ID exists
+    echo "MapR user password: $MAPR_USER_PASSWORD"
+else
+    echo 'MAPR_USER_PASSWORD environment variable is not set. Please set it and rerun.'
+    exit 1
+fi
+
 sudo /opt/mapr/server/configure.sh -R -c -Z ${MAPR_CLDB_HOSTS}:5181 -C ${MAPR_CLDB_HOSTS}:7222
 sudo chown ${MAPR_CONTAINER_USER}:${MAPR_CONTAINER_GROUP} -R /opt/mapr/spark
-sudo chown ${MAPR_CONTAINER_USER}:${MAPR_CONTAINER_GROUP} -R /opt/mapr/hive
+sudo chown ${MAPR_CONTAINER_USER}:${MAPR_CONTAINER_GROUP} -R /opt/mapr/drill
 
 airflow initdb
 SPARK_VERSION=$(cat /opt/mapr/spark/sparkversion)
+DRILL_VERSION=$(cat /opt/mapr/drill/drillversion)
 export PATH=$PATH:/opt/mapr/spark/spark-${SPARK_VERSION}/bin
+export PATH=$PATH:/opt/mapr/drill/drill-${DRILL_VERSION}/bin
 export MAPR_DAG_SPARK_JOB_PATH=/home/mapr/mapr-apps/spark-statistics-job-1.0.0-SNAPSHOT.jar
 export MAPR_DAG_DRILL_SCRIPT_PATH=/home/mapr/airflow/bin/drill-script.sh
 
 cp -R /home/mapr/mapr-apps/mapr-airflow/* /home/mapr/airflow/
 
-# TODO: add GitHub connection, configure Hive, Drill connections
+# Set Drill node name
+sed -i -e "s/localhost/$DRILL_NODE/g" /home/mapr/airflow/bin/drill-script.sh
+
+# Change drill-override.conf
+sed -i -e "s/localhost:2181/$DRILL_NODE:5181/g" /opt/mapr/drill/drill-${DRILL_VERSION}/conf/drill-override.conf
+sed -i -e "s/drillbits1/$DRILL_CLUSTER_ID/g" /opt/mapr/drill/drill-${DRILL_VERSION}/conf/drill-override.conf
+
+# Create GitHub HTTP connection, which will be used by 'mapr_tasks_dag'
+airflow connections --add --conn_id http_github --conn_type http --conn_host https://api.github.com --conn_schema https
+
+# Edit Hive connection to use Beeline instead of Hive CLI
+airflow connections --delete --conn_id hive_cli_default
+airflow connections --add --conn_id hive_cli_default --conn_type hive_cli --conn_host node1 --conn_port 10000 --conn_schema default --conn_login ${MAPR_CONTAINER_USER} --conn_password ${MAPR_USER_PASSWORD} --conn_extra "{\"use_beeline\": true, \"auth\": \"null;user=$MAPR_CONTAINER_USER;password=$MAPR_USER_PASSWORD\"}"
 
 airflow scheduler &
 
